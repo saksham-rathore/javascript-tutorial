feaae1b0e258da649b241456dffd7f7daf02a3c9
saksham762007 <samrathore07062007@gmail.com>
2025-12-05 18:15:00 +0530

    file updated

--- first.js
+++ first.js
@@ -4,3 +4,23 @@ x = null;  // value is empty
 y = undefined; // you dont know there value 
 isFollow = false; //boolean value 
 console.log(isFollow);
+
+
+/* it is not redeclarable but can be updated */
+let name = "sam developer";  
+console.log(name);
+
+
+
+// var is a old method to use declare a variable in js
+// it is redeclarable and updated method
+var age = 22;
+console.log(age)
+
+// variable cannot be redeclarable or updated
+const age = 244
+console.log(age)
+
+
+
+
