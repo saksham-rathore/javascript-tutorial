22ff92caf8eb961d3d24839f8d856fe50c87322c
saksham762007 <samrathore07062007@gmail.com>
2025-12-05 16:46:25 +0530

    file updated

--- first.js
+++ first.js
@@ -1,1 +1,6 @@
-console.log("I am learning javascript");
+fullName = "sam";
+Age = 23;
+x = null;  // value is empty 
+y = undefined; // you dont know there value 
+isFollow = false; //boolean value 
+console.log(isFollow);
