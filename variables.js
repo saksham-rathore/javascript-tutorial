71ce68a2c517e5fde131bff546bd2f02408327bf
saksham762007 <samrathore07062007@gmail.com>
2025-12-05 17:11:54 +0530

    file updated

--- variables.js
+++ variables.js
@@ -2,3 +2,15 @@ fullname = "sam rathore";
 fullName = "sam rathore";
 
 console.log(fullname);
+console.log(fullName);
+
+//full name = "sam rat"  //not allowed
+
+_fullname = "sam rathore";
+//234fullname = "sam rathore"; //not allowed 
+
+// reserved
+
+// console = "sam rathore";
+// console.log(console);
+// console is a reserved word in js and cannot be used as a variable name.
