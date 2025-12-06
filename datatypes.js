0dc9a53a95d65f485225171833d03f0e1dfa6e56
saksham762007 <samrathore07062007@gmail.com>
2025-12-06 12:11:50 +0530

    file updated

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+// primitive data types 
+// number type 
+let age = 23
+
+
+// string type 
+let name = "sam"
+
+// boolean type 
+isfollow = true
+
+// undefimnmed type 
+let x;
+
+// null type 
+let y = null;
+
+
+// in primitive data types is collection of different variables key value pairs 
+const student = {
+    fullname: "rathore",
+    skills: "data scientist",
+    age: 19,
+    isfollow: true
+};
+
+// update value 
+student.age = student.age + 1;
+console.log(student.age)
