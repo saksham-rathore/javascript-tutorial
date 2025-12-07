8f386a6efc15620974c5e6e54102171cc3a6e85f
saksham762007 <samrathore07062007@gmail.com>
2025-12-07 22:38:32 +0530

    file update

--- loops.js
+++ loops.js
@@ -1,9 +1,20 @@
-for (let count = 1; count <= 100; count++) {
-    console.log("hey! sam");
+/* print 1 to 5 
+for (let i = 1; i <= 5; i++) {
+console.log("i = ", i);
 }
+
 
 
 let i = 1;
-while(i<=5) {
-    console.log("i=",i);
+while (i <= 10) {
+console.log("hey sam");
+i++
 }
+
+conditon apply at least one time is mandatory 
+let i = 20;
+do {
+console.log("hey sam"); i++;
+}while(i <= 10);
+
+/*
