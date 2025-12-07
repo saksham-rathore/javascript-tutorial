1ce4a762eb243349003576510e71030931742b91
saksham762007 <samrathore07062007@gmail.com>
2025-12-07 23:02:28 +0530

    file update

--- loops.js
+++ loops.js
@@ -17,4 +17,25 @@ do {
 console.log("hey sam"); i++;
 }while(i <= 10);
 
-/*
+
+apply loop in one character which is arrays or strings 
+for-of loop 
+let str = "hey sam";
+
+for(let i of str) {
+console.log("i=", i);
+}
+
+
+for in loop which is work to return a key values 
+
+let student = {
+name: "sam rathore",
+age: 20
+cgpa: 9.5,
+ispass: true,
+};
+
+for (let i in student) {
+console.log(i);
+}
