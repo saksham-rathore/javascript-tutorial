97c329b8bfe96b7b50b28b6857156c2e80bbc751
saksham762007 <samrathore07062007@gmail.com>
2025-12-10 12:03:44 +0530

    initial commit

---
+++ for_each_loop_array.js
@@ -0,0 +1,12 @@
+function abc() {
+    console.log("hello");
+}
+function myfunction(abc) {
+    return abc;
+}
+
+let arr = ["pune", "banglore", "mumbai"];
+
+arr.forEach((val) => {
+    console.log(val);
+});
