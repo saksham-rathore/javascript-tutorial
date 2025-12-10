ce99aa1df305a9cfa04eb98efcf8f9269cedeaab
saksham762007 <samrathore07062007@gmail.com>
2025-12-10 15:04:57 +0530

    file updated

--- for_each_loop_array.js
+++ for_each_loop_array.js
@@ -5,8 +5,37 @@ function myfunction(abc) {
     return abc;
 }
 
+// for each loop in array
 let arr = ["pune", "banglore", "mumbai"];
 
 arr.forEach((val) => {
     console.log(val);
 });
+
+// some more array map method
+let nums = [2, 423, 324];
+
+let newarr = nums.map((val) => {
+    return val;
+});
+ 
+console.log(newarr);
+
+
+// filter array
+let arr2 = [1,2,3,4,5,6,7];
+
+let evenArr = arr2.filter((val) => {
+    return val % 2 === 0;
+});
+
+console.log(evenArr);
+
+// reduce method 
+let arr3 = [1,2,3,4];
+ 
+const output = arr.reduce((res, curr) => {
+    return res + curr;
+});
+
+console.log(output);
