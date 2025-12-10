6fca533b75f703c8563422b16027da08e55ba5b4
saksham762007 <samrathore07062007@gmail.com>
2025-12-10 11:40:14 +0530

    initial commit

---
+++ functions.js
@@ -0,0 +1,26 @@
+function myfunction() {
+    console.log("welcome to javascript");
+    console.log("we are learning js :");
+}
+
+myfunction();
+
+
+function myfunction(msg) {
+    // parameter -> input
+    console.log(msg);
+}
+
+myfunction("i love js") //argument
+
+
+// functions -> 2 nmbers, sum 
+function sum(x, y) {
+    console.log(x+y);
+}
+
+// modern javascript
+const arrowsun = (a, b) => {
+    console.log(a + b);
+}
+
