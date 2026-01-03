13a101d056c796550018560cecadd0d51b2bfb1a
saksham762007 <samrathore07062007@gmail.com>
2026-01-04 02:58:38 +0530

    file updated

---
+++ objects.js
@@ -0,0 +1,15 @@
+// function function_name(parameter1, parameter2) {
+//     code to run 
+//     return value;
+// }
+
+// function_name(arguments1, arguments2)
+
+function add(a,b) {
+    let sum = a + b;
+    return sum;
+}
+
+let result = add(10,49)
+console.log(result)
+ 
