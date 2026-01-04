0f7a379b4377091c4d900e824ced05d4efd0a011
saksham762007 <samrathore07062007@gmail.com>
2026-01-04 23:55:15 +0530

    initial commit

--- functions.js
+++ functions.js
@@ -24,3 +24,29 @@ const arrowsun = (a, b) => {
     console.log(a + b);
 }
 
+
+// function function_name(parameter1, parameter2) {
+//     code to run 
+//     return value;
+// }
+
+// function_name(arguments1, arguments2)
+
+
+/*
+function add(a,b) {
+    let sum = a + b;
+    return sum;
+}
+let result = add(10,49)
+console.log(result)
+ 
+*/
+
+function calculatediscount(price, discountprice) {
+    let discount = price * (discountprice / 100);
+    let final_price = price - discount;
+    return final_price;
+}
+
+console.log(calculatediscount(1000, 20));
