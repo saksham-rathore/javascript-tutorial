af73c2c4e074df17f471e413b5194494a396cc11
saksham762007 <samrathore07062007@gmail.com>
2026-01-05 23:19:45 +0530

    file updates

---
+++ DOM manupulation/script.js
@@ -0,0 +1,9 @@
+let sam = document.getElementById("sam");
+console.dir(sam);
+
+
+
+// i have learn document.queryselector
+// i have learn document.queryselectorall
+// i have learn document.getElementById
+// i have learn document.getElementByclassname
