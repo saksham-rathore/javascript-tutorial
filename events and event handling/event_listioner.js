374bcbd37706376e507a37b75b177d80d98725ca
saksham762007 <samrathore07062007@gmail.com>
2026-01-08 01:49:21 +0530

    file updated

--- events and event handling/event_listioner.js
+++ events and event handling/event_listioner.js
@@ -1,3 +1,5 @@
+/*
+make a downloading counter
 let conut = 0;
 let seconds = 10;
 let progress = document.querySelector(".progress-bar");
@@ -16,3 +18,25 @@ let intv = setInterval(
     },
     (seconds * 1000) / 100
 );
+*/
+
+
+/*
+local storeage -> aapke browser ke ander data store karna jo ki browser band hone per bhi delete nhi hoga 
+
+sessionstorage -> ye aapke data temporarily store karta hai matlab ki tab band hua or data gya
+
+
+cookies -> ye bhi data store karta hai and aapke data browser ke cookies naam ki property main save hota hai and  ye cookie concept kam data ya light data use ke liye hota hai
+
+
+cookies
+browser mai chota data store karne ke liye cookies ka istemaal hota hai 
+
+
+~ cookies - 4kb
+local storage or SessionStorage - ~5mb
+
+local storage mai array save nhi kr sakte hai jisko hame json property se convert krna padega array form mai 
+
+*/
