3eb09d8901f55a3eee0cab8e354eaad265e9b4a0
saksham762007 <samrathore07062007@gmail.com>
2026-01-07 22:03:07 +0530

    file updated

--- objects/events and event handling/event_listioner.js
+++ objects/events and event handling/event_listioner.js
@@ -1,39 +1,6 @@
-let h1 = document.querySelector("h1");
+let box = document.querySelector("#box");
 
-h1.addEventListener("click", function (){
-    h1.style.color = "red";
-})
-
-/*
-it works to tap in h1 to change a color it
-works through the listioner
-
-
-element.addEventListener("event name", function(){
-
-remove eventlistioner is 
-h1.removeenentlistioner(mdsonfonsdo)
-
-})
-*/
-
-// this is input event
-let inp = document.querySelector("input");
-
-inp.addEventListener("input", function (dets){
-    if (dets.data !== null) {
-        console.log(dets.data);
-    }
+window.addEventListener("mousemove", function (dets) {
+    box.style.top = dets.clientY + "px";
+    box.style.left = dets.clientX + "px";
 });
-
-
-// change event jab chalta hai jab aapke koi input ya select area mai koi change aajaye 
-
-let sel = document.querySelector("select");
-let device = document.querySelector("#device");
-
-sel.addEventListener("change", function (e) {
-  console.log(e.target.value);
-  device.value = `${e.target.value} device selected`;
-});
-
